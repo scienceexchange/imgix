f80b94706039945ed138149de72f1f2828eafe14
Michael Kompanets <mkompanets@gmail.com>
2014-01-20 14:14:03 -0800

    removing options limitation

--- lib/imgix/style.rb
+++ lib/imgix/style.rb
@@ -1,17 +1,13 @@
 module Imgix
   class Style
     attr_reader :name, :options
-    
+
     def initialize(name, attachment, options = {})
       @name       = name
       @attachment = attachment
       @options    = options
-      
-      if @options.is_a?(Hash)
-        @options = @options.delete_if {|o| ![:h, :w, :crop, :fit, :bg].include?(o) }
-      end
     end
-    
+
     def to_s
       @options.to_param
     end
