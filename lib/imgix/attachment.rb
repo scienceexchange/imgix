d6a53f923a34695cd670e6a269f7d58e5f523c91
Michael Kompanets <mkompanets@gmail.com>
2014-03-27 15:45:34 -0700

    default to specified image when file_name is a blank string

--- lib/imgix/attachment.rb
+++ lib/imgix/attachment.rb
@@ -3,11 +3,11 @@ module Imgix
     attr_reader :name, :instance, :options
 
     def initialize(name, instance, params = {})
-      @name     = name
-      @instance = instance
       @params   = self.class.default_options.merge(params)
+      @name     = name.present? ? name : @params[:default]
+      @instance = instance
     end
-    
+
     def url(style = nil)
       base_url      = "#{Imgix.options[:protocol]}://#{@instance.options[:subdomain]}.#{Imgix.options[:base_url]}"
 
@@ -19,32 +19,32 @@ module Imgix
         full_url
       end
     end
-    
+
     def file_name
-      @instance.send("#{@name}_file_name") || @params[:default]
+      @instance.send("#{@name}_file_name")
     end
-    
+
     def content_type
       @instance.send("#{@name}_content_type")
     end
-    
+
     def file_size
       @instance.send("#{@name}_file_size")
     end
-    
+
     def default_style
       @options[:default_style]
     end
-    
-    def styles    
+
+    def styles
       @normalized_styles = @params[:styles].dup
       @normalized_styles.each_pair do |name, options|
         @normalized_styles[name.to_sym] = Imgix::Style.new(name, self, options.dup)
       end
-      
+
       @normalized_styles
     end
-    
+
     def self.default_options
       @default_options ||= {
         :default_style  => :original,
